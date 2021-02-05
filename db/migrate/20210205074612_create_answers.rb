acbbfe71bbd52355ed67e42191b0bc1c23d75b20
Ivan Pushkin <naveroot@gmail.com>
2021-02-05 10:59:29 +0300

    add correct field to model answer

--- db/migrate/20210205074612_create_answers.rb
+++ db/migrate/20210205074612_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.string :body, null: false
+      t.boolean :correct, default: false, null: false
 
       t.timestamps
     end

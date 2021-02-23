5363ce586b5a4566f50d584099f5e848456352c7
naveroot <naveroot@gmail.com>
2021-02-23 10:21:35 +0300

    add associations to models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+    has_many :test_passages
+    has_many :tests, through: :test_passages
+    has_many :questions
+    belongs_to :category, optional: true
+    belongs_to :author, class_name: 'User'
+
     def self.titles_by_category(category)
         joins('JOIN categories ON categories.id = tests.categories_id')
         .where(categories: { title: category})
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+    has_many :test_passages
+    has_many :tests, through: :test_passages
+    has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
+    
     def tests_with_level(level)
         Test.joins('JOIN test_passages ON tests.id = test_passages.test_id')
             .where('test_passages.user_id = :user_id AND level = :level', user_id: id, level: level)
-      end
+    end
 end

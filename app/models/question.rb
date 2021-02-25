44e053b0bbae7092ff28c98e4c96050d19ba3673
Ivan Pushkin <naveroot@gmail.com>
2021-02-25 15:07:24 +0300

    fix after review

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,4 @@
 class Question < ApplicationRecord
-    MAXIMUM_NUMBER_OF_ANSWERS = 4
     
     belongs_to :test
     has_many :answers
--- app/models/test.rb
+++ app/models/test.rb
@@ -8,12 +8,12 @@ class Test < ApplicationRecord
     scope :easy, -> { where(level: 0..1) }
     scope :medium, -> { where(level: 2..4) }
     scope :hard, -> { where(level: 5..Float::INFINITY) }
-    scope :by_category_desc, ->(category) { joins(:category).where(categories: { title: category }).order(title: :desc) }
+    scope :by_category, ->(category) { joins(:category).where(categories: { title: category }) }
 
     validates :title, presence: true, uniqueness: { scope: :level }
     validates :level, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
     def self.titles_by_category(category)
-        by_category_desc(category).pluck(:title)
+        by_category(category).pluck(:title)
     end
 end

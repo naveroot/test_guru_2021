0b76c45e459c43f9a1a995bc84de008429c96a7b
naveroot <naveroot@gmail.com>
2021-03-03 11:00:54 +0300

    Question resources and controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,55 @@
+class QuestionsController < ApplicationController
+    before_action :find_question, only: %i[show destroy]
+    before_action :find_test, only: %i[index new create]
+  
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_question_not_found
+  
+    def index
+      @questions = @test.questions
+      responce = ["Questions for test \"#{@test.title}\":\n"]
+      @questions.each_with_index { |question, index| responce << "#{index + 1}.  #{question.body}\n" }
+      render plain: responce.join
+    end
+  
+    def show
+      render plain: @question.body
+    end
+  
+    def new
+      @question = Question.new
+    end
+  
+    def create
+      @question = Question.new(question_params)
+      @question.test = @test
+      if @question.save
+        redirect_to test_questions_path(@question.test)
+      else
+        render plain: "Some error"
+      end
+  end
+
+
+  def destroy
+    @question.destroy!
+    redirect_to test_questions_path(@question.test)
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_from_question_not_found
+    render plain: 'Question not found'
+  end
+end
